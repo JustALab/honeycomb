843cc993ead1b195bf2cf656f48c0c1b09ff84a3
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-01 18:34:22 +0530

    user- auth test data commit

--- test data/user-auth-data.sql
+++ test data/user-auth-data.sql
@@ -34,9 +34,9 @@ INSERT INTO `authority` (`id`, `name`) VALUES
 --
 
 INSERT INTO `user` (`id`, `email`, `enabled`, `firstname`, `lastpasswordresetdate`, `lastname`, `password`, `username`) VALUES
-(1, 'admin@admin.com', b'1', 'admin', '2016-01-01 00:00:00', 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin'),
-(2, 'user@user.com', b'1', 'user', '2016-01-01 00:00:00', 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user'),
-(3, 'vendor@user.com', b'1', 'vendor', '2016-01-01 00:00:00', 'vendor', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'vendor');
+(1, 'admin@admin.com', b'1', 'admin', '2016-01-01 00:00:00', 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'a@a.com'),
+(2, 'user@user.com', b'1', 'user', '2016-01-01 00:00:00', 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'u@u.com'),
+(3, 'vendor@user.com', b'1', 'vendor', '2016-01-01 00:00:00', 'vendor', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'v@v.com');
 
 --
 -- Dumping data for table `user_authority`
