37f75a8f2bf709e64affa11f4c9bdb5cbbbf64b2
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-02 21:49:41 +0530

    test data customer

--- test data/customers.sql
+++ test data/customers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 28, 2018 at 04:59 PM
+-- Generation Time: Apr 02, 2018 at 06:19 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 --
 
 INSERT INTO `customers` (`customer_id`, `email`, `first_name`, `last_name`, `mobile`, `signup_date`) VALUES
-(1, 'dummy_user1@gmail.com', 'Dummy', 'User 1', '9876543210', '2018-03-28 20:22:00');
+(1, 'u@u.com', 'user', 'user', '9876543210', '2018-03-28 20:22:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
