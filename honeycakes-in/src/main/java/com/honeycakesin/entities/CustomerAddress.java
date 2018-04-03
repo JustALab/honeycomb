64665314bacef7e5a0ef3a3ca8ad81c0b1221be3
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-03 23:03:49 +0530

    CustomerAddress JsonIgnore

--- honeycakes-in/src/main/java/com/honeycakesin/entities/CustomerAddress.java
+++ honeycakes-in/src/main/java/com/honeycakesin/entities/CustomerAddress.java
@@ -13,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.honeycakesin.constants.DeliveryToAddressType;
 
 import lombok.AccessLevel;
@@ -30,6 +31,7 @@ public class CustomerAddress implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long addressId;
 	
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "customer_id")
 	Customer customer;
