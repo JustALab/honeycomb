f1c0106ad5580699397727eff3304be1b462555d
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-03 19:19:34 +0530

    JwtUser comment removed

--- honeycakes-in/src/main/java/com/honeycakesin/security/JwtUser.java
+++ honeycakes-in/src/main/java/com/honeycakesin/security/JwtUser.java
@@ -8,9 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * Created by stephan on 20.03.16.
- */
 @SuppressWarnings("serial")
 public class JwtUser implements UserDetails {
 
