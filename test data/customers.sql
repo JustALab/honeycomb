da515aea220322ec979a32a4c26aba1c0b0cfa38
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-03 23:24:53 +0530

    customer test data updated

--- test data/customers.sql
+++ test data/customers.sql
@@ -3,18 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 02, 2018 at 06:19 PM
+-- Generation Time: Apr 03, 2018 at 07:53 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `honeycomb`
@@ -24,9 +18,5 @@ SET time_zone = "+00:00";
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`customer_id`, `email`, `first_name`, `last_name`, `mobile`, `signup_date`) VALUES
-(1, 'u@u.com', 'user', 'user', '9876543210', '2018-03-28 20:22:00');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `customers` (`customer_id`, `email`, `first_name`, `last_name`, `mobile`, `signup_date`, `email_verification_status`, `mobile_verification_status`) VALUES
+(1, 'u@u.com', 'user', 'user', '9876543210', '2018-03-28 20:22:00', 'NOT_VERIFIED', 'NOT_VERIFIED');
