bafc936fb04c99d22853b47e27dec9e817b8cf0b
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-04 13:22:24 +0530

    auth relocation updates

--- honeycakes-in/src/main/java/com/honeycakesin/auth/config/WebSecurityConfig.java
+++ honeycakes-in/src/main/java/com/honeycakesin/auth/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.honeycakesin.auth;
+package com.honeycakesin.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.honeycakesin.auth.JwtAuthenticationEntryPoint;
+import com.honeycakesin.auth.JwtAuthorizationTokenFilter;
+import com.honeycakesin.auth.JwtTokenUtil;
 import com.honeycakesin.service.JwtUserDetailsService;
 
 @Configuration
