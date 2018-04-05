b9efa85245f3192dcbd0298f51fa110c41ee867b
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-06 01:08:38 +0530

    test data changes commit

--- test data/businness-data.sql
+++ test data/businness-data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 28, 2018 at 05:18 PM
+-- Generation Time: Mar 28, 2018 at 04:59 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
@@ -21,8 +21,29 @@ SET time_zone = "+00:00";
 --
 
 --
--- Dumping data for table `vendor_items`
+-- Dumping data for table `vendors`
 --
+
+INSERT INTO `customers` (`customer_id`, `email`, `first_name`, `last_name`, `mobile`, `signup_date`, `email_verification_status`, `mobile_verification_status`) VALUES
+(1, 'u@u.com', 'user', 'user', '9876543210', '2018-03-28 20:22:00', 'NOT_VERIFIED', 'NOT_VERIFIED');
+
+INSERT INTO `vendors` (`vendor_id`, `address`, `contact_email`, `contact_mobile`, `vendor_name`) VALUES
+(1, 'Thiruvanmiyur', 'vendora@gmail.com', '9876543210', 'Vendor A'),
+(2, 'Karapakkam', 'vendorb@gmail.com', '9876543211', 'Vendor B'),
+(3, 'Sholinganallur', 'vendorc@gmail.com', '9876543212', 'Vendor C'),
+(4, 'Navallur', 'vendord@gmail.com', '9876543213', 'Vendor D');
+
+INSERT INTO `items` (`item_id`, `item_category`, `item_name`, `item_price`, `quantity_slab`) VALUES
+(1, 'CAKE', 'Vanilla', 400, 1),
+(2, 'CAKE', 'Strawberry', 450, 1),
+(3, 'CAKE', 'Chocolate', 600, 1),
+(4, 'CAKE', 'Choco Truffle', 800, 1),
+(5, 'CAKE', 'Black Forest', 850, 1),
+(6, 'CAKE', 'White Forest', 800, 1),
+(7, 'PARTY_PACK', 'Party Hat', 20, 1),
+(8, 'PARTY_PACK', 'Snow Spray', 80, 1),
+(9, 'PARTY_PACK', 'Candle', 5, 1),
+(10, 'PARTY_PACK', 'Fancy Candle', 20, 1);
 
 INSERT INTO `vendor_items` (`vendor_items_id`, `availability_status`, `item_id`, `vendor_id`) VALUES
 (1, 'AVAILABLE', 1, 1),
@@ -65,6 +86,16 @@ INSERT INTO `vendor_items` (`vendor_items_id`, `availability_status`, `item_id`,
 (38, 'AVAILABLE', 8, 4),
 (39, 'AVAILABLE', 9, 4),
 (40, 'AVAILABLE', 10, 4);
+
+INSERT INTO `locations` (`location_id`, `location_name`, `vendor_id`) VALUES
+(1, 'Thiruvanmiyur', 1),
+(2, 'SRP Tools', 1),
+(3, 'Perungudi', 2),
+(4, 'Thoraipakkam', 2),
+(5, 'Karapakkam', 2),
+(6, 'Sholinganallur', 3),
+(7, 'Navallur', 4),
+(8, 'Siruseri', 4);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
