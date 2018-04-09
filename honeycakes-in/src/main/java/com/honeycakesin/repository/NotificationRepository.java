eccb79a806cb81a5100e3ff972cd8859e02dae4b
Ramu Ramasamy <rram.ramasamy@gmail.com>
2018-04-10 00:15:14 +0530

    notification repository query change

--- honeycakes-in/src/main/java/com/honeycakesin/repository/NotificationRepository.java
+++ honeycakes-in/src/main/java/com/honeycakesin/repository/NotificationRepository.java
@@ -6,14 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.honeycakesin.constants.NotificationType;
 import com.honeycakesin.entities.Notification;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-	String FIND_BY_CUSTOMER_MOBILE = "SELECT n FROM Notification n WHERE n.userMobile = ?1 AND n.notificationType = \"OTP\" ORDER BY n.notificationDateTime DESC";
+	String FIND_BY_CUSTOMER_MOBILE = "SELECT n FROM Notification n WHERE n.userMobile = ?1 AND n.notificationType = ?2 ORDER BY n.notificationDateTime DESC";
 
 	@Query(FIND_BY_CUSTOMER_MOBILE)
-	List<Notification> findByCustomerMobile(String mobile);
+	List<Notification> findByCustomerMobile(String mobile, NotificationType notificationType);
 
 }
